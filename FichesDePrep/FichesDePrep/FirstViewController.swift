c43459e2901900fcc81afb9a44bc87d59056afd3
Chloë Moulinet <chloe.moulinet@flexy.co>
2020-09-22 10:13:04 +0200

    Init iOS project

---
+++ FichesDePrep/FichesDePrep/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  FichesDePrep
+//
+//  Created by Chloe Moulinet on 22/09/2020.
+//  Copyright © 2020 Chloe Moulinet. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FichesDePrep/FichesDePrep/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  FichesDePrep
+//
+//  Created by Chloe Moulinet on 22/09/2020.
+//  Copyright © 2020 Chloe Moulinet. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
