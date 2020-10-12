a988b34fc1f168527123e00c9bc46f285c1e90cc
Moulinet Chloë <cmoulinet@gmail.com>
2020-10-12 08:42:09 +0200

    Add iQKeyboard

--- FichesDePrep/FichesDePrep/AppDelegate.swift
+++ FichesDePrep/FichesDePrep/AppDelegate.swift
@@ -8,12 +8,16 @@
 
 import UIKit
 import RealmSwift
+import IQKeyboardManagerSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        IQKeyboardManager.shared.enable = true
+
         do {
             try FileManager.default.removeItem(at: Realm.Configuration.defaultConfiguration.fileURL!)
         } catch {
