49b7098bb5a2557e8479daf4cd9e0ef8a1a710c9
Chloë Moulinet <chloe.moulinet@flexy.co>
2020-10-17 22:29:30 +0200

    Fix height for phases

--- FichesDePrep/FichesDePrep/Core/PDFGenerator.swift
+++ FichesDePrep/FichesDePrep/Core/PDFGenerator.swift
@@ -33,6 +33,8 @@ class PDFGenerator {
         let renderer = UIGraphicsPDFRenderer(bounds: pageRect, format: format)
         let data = renderer.pdfData { (context) in
             context.beginPage()
+            
+            
             let domainPosition = addText(ofWidth: pageWidth,
                                        entitled: "Domaine d'activité: ",
                                        withText: prepFileData.activityKind)
@@ -79,38 +81,33 @@ class PDFGenerator {
                                          under: specificGoalPosition.0)
 
             
+            
             let bottomWidth = pageWidth - 25
             let phasePosition = addText(ofWidth: bottomWidth * 0.075,
-                                        andHeight: pageHeight - materialPosition.0,
                                         entitled: "Phase\n",
                                         withText: prepFileData.phases?.list[0].phaseNumber.description ?? "",
                                         under: materialPosition.0 + 15)
             let consignePosition = addText(ofWidth: bottomWidth * 0.400,
-                                           andHeight: pageHeight - materialPosition.0,
                                            entitled: "Consigne\n",
                                            withText: prepFileData.phases?.list[0].consigne ?? "",
                                            under: materialPosition.0 + 15,
                                            next: phasePosition.1 + 5)
             let phaseDurationPosition = addText(ofWidth: bottomWidth * 0.05,
-                                           andHeight: pageHeight - materialPosition.0,
                                            entitled: "Durée\n",
                                            withText: "\(prepFileData.phases?.list[0].phaseDuration) min",
                                            under: materialPosition.0 + 15,
                                            next: consignePosition.1 + 5)
             let teacherRolePosition = addText(ofWidth: bottomWidth * 0.175,
-                                              andHeight: pageHeight - materialPosition.0,
                                               entitled: "Rôle de l'enseignant\n",
                                               withText: prepFileData.phases?.list[0].teacherRole ?? "",
                                               under: materialPosition.0 + 15,
                                               next: phaseDurationPosition.1 + 5)
             let pupilRolePosition = addText(ofWidth: bottomWidth * 0.150,
-                                            andHeight: pageHeight - materialPosition.0,
                                             entitled: "Rôle de l'élève\n",
                                             withText: prepFileData.phases?.list[0].pupilRole ?? "",
                                             under: materialPosition.0 + 15,
                                             next: teacherRolePosition.1 + 5)
             let _ = addText(ofWidth: bottomWidth * 0.150,
-                            andHeight: pageHeight - materialPosition.0,
                             entitled: "Différenciation\n",
                             withText: prepFileData.phases?.list[0].differenciation ?? "",
                             under: materialPosition.0 + 15,
@@ -139,50 +136,51 @@ class PDFGenerator {
             // Bottom section - Top line
             drawLine(context.cgContext, startX: 10, endX: pageRect.width - 10, startY: materialPosition.0 + 13, endY: materialPosition.0 + 13)
             // Bottom section - Bottom line
-            drawLine(context.cgContext, startX: 10, endX: pageRect.width - 10, startY: phasePosition.0 + 2, endY: phasePosition.0 + 2)
+            drawLine(context.cgContext, startX: 10, endX: pageRect.width - 10, startY: pageRect.height - 10, endY: pageRect.height - 10)
             // Bottom section - Left line
-            drawLine(context.cgContext, startX: 10, endX: 10, startY: materialPosition.0 + 13, endY: phasePosition.0 + 2)
+            drawLine(context.cgContext, startX: 10, endX: 10, startY: materialPosition.0 + 13, endY: pageRect.height - 10)
             // Bottom section - Right ine
-            drawLine(context.cgContext, startX: pageRect.width - 10, endX: pageRect.width - 10, startY: materialPosition.0 + 13, endY: phasePosition.0 + 2)
+            drawLine(context.cgContext, startX: pageRect.width - 10, endX: pageRect.width - 10, startY: materialPosition.0 + 13, endY: pageRect.height - 10)
             
             // Bottom section - Phase / Consigne
-            drawLine(context.cgContext, startX: phasePosition.1 + 2, endX: phasePosition.1 + 2, startY: materialPosition.0 + 13, endY: phasePosition.0 + 2)
+            drawLine(context.cgContext, startX: phasePosition.1 + 2, endX: phasePosition.1 + 2, startY: materialPosition.0 + 13, endY: pageRect.height - 10)
             // Bottom section - Phase / Consigne
-            drawLine(context.cgContext, startX: consignePosition.1 + 2, endX: consignePosition.1 + 2, startY: materialPosition.0 + 13, endY: consignePosition.0 + 2)
+            drawLine(context.cgContext, startX: consignePosition.1 + 2, endX: consignePosition.1 + 2, startY: materialPosition.0 + 13, endY: pageRect.height - 10)
             // Bottom section - Phase / Consigne
-            drawLine(context.cgContext, startX: phaseDurationPosition.1 + 2, endX: phaseDurationPosition.1 + 2, startY: materialPosition.0 + 13, endY: phaseDurationPosition.0 + 2)
+            drawLine(context.cgContext, startX: phaseDurationPosition.1 + 2, endX: phaseDurationPosition.1 + 2, startY: materialPosition.0 + 13, endY: pageRect.height - 10)
             // Bottom section - Phase / Consigne
-            drawLine(context.cgContext, startX: teacherRolePosition.1 + 2, endX: teacherRolePosition.1 + 2, startY: materialPosition.0 + 13, endY: teacherRolePosition.0 + 2)
+            drawLine(context.cgContext, startX: teacherRolePosition.1 + 2, endX: teacherRolePosition.1 + 2, startY: materialPosition.0 + 13, endY: pageRect.height - 10)
             // Bottom section - Phase / Consigne
-            drawLine(context.cgContext, startX: pupilRolePosition.1 + 2, endX: pupilRolePosition.1 + 2, startY: materialPosition.0 + 13, endY: pupilRolePosition.0 + 2)
+            drawLine(context.cgContext, startX: pupilRolePosition.1 + 2, endX: pupilRolePosition.1 + 2, startY: materialPosition.0 + 13, endY: pageRect.height - 10)
         }
 
         return data
     }
     
-    func addText(ofWidth: CGFloat, andHeight: CGFloat? = nil, entitled: String, withText text: String, under bottomPosition: CGFloat = 12, next rightPosition: CGFloat = 12) -> (CGFloat, CGFloat) {
+    func addText(ofWidth: CGFloat, entitled: String, withText text: String, under bottomPosition: CGFloat = 12, next rightPosition: CGFloat = 12) -> (CGFloat, CGFloat) {
 
         let textFont = UIFont.systemFont(ofSize: 12.0, weight: .regular)
         let boldFont = UIFont.systemFont(ofSize: 12.0, weight: .bold)
-
-        let entitledAttributes: [NSAttributedString.Key: Any] =
-            [NSAttributedString.Key.font: boldFont]
-        let entitledText = NSMutableAttributedString(string: entitled, attributes: entitledAttributes)
-
         let paragraphStyle = NSMutableParagraphStyle()
         paragraphStyle.alignment = .left
         paragraphStyle.lineBreakMode = .byWordWrapping
+
+        let entitledAttributes: [NSAttributedString.Key: Any] =
+            [NSAttributedString.Key.font: boldFont,
+             NSAttributedString.Key.paragraphStyle: paragraphStyle]
+        let entitledText = NSMutableAttributedString(string: entitled, attributes: entitledAttributes)
+
         let textAttributes: [NSAttributedString.Key: Any] =
             [NSAttributedString.Key.font: textFont,
              NSAttributedString.Key.paragraphStyle: paragraphStyle]
         let attributedText = NSMutableAttributedString(string: text, attributes: textAttributes)
         
         entitledText.append(attributedText)
-        let textStringSize = entitledText.size()
+        let textStringSize = entitledText.string.size(withAttributes: textAttributes, width: ofWidth)
         let textStringRect = CGRect(x: rightPosition,
                                     y: bottomPosition,
                                     width: ofWidth,
-                                    height: andHeight == nil ? textStringSize.height : andHeight!)
+                                    height: textStringSize.height)
         
         entitledText.draw(in: textStringRect)
         return (textStringRect.origin.y + textStringRect.size.height, textStringRect.origin.x + textStringRect.size.width)
@@ -199,5 +197,14 @@ class PDFGenerator {
         drawContext.restoreGState()
         
         drawContext.saveGState()
+    }
+}
+
+extension String {
+    func size(withAttributes attributes: [NSAttributedString.Key: Any], width: CGFloat) -> CGSize {
+    let attrString = NSAttributedString(string: self, attributes: attributes)
+        let bounds = attrString.boundingRect(with: CGSize(width: width, height: .greatestFiniteMagnitude), options: .usesLineFragmentOrigin, context: nil)
+        let size = CGSize(width: bounds.width, height: bounds.height)
+        return size
     }
 }
